16c2be538c99b2e1d3c316401d7b7dcc7a16bdcb
sunnysingh100 <sunnysingh706050@gmail.com>
2024-03-02 07:02:24 +0530

    add tailwind

--- 01Counter/src/App.jsx
+++ 01Counter/src/App.jsx
@@ -16,13 +16,23 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="flex flex-col items-center justify-center w-full h-screen ">
+      <h1 className="text-3xl text-red-600">Hello</h1>
       <h2>Name: {name}</h2>
       <h2>Value: {value}</h2>
-      <button onClick={icrease}>Increase</button>
+      <button
+        className="p-4 m-6 bg-green-400 rounded-3xl hover:bg-red-300"
+        onClick={icrease}
+      >
+        Increase
+      </button>
       <br />
-      <button onClick={decrease}>Decrease</button>
+      <button
+        className="p-4 bg-green-100 rounded-3xl hover:bg-blue-300"
+        onClick={decrease}
+      >
+        Decrease
+      </button>
     </div>
   );
 }
